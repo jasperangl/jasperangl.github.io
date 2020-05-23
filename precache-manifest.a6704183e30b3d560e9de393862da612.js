6d8ee7c98b39a2c746503c0c5734cc3e054fe13b
jasperangl <49351257+jasperangl@users.noreply.github.com>
2020-05-23 18:09:16 +0200

    Updates

---
+++ precache-manifest.a6704183e30b3d560e9de393862da612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd30df9b950e917054c0db57198cd97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e86c848160a82e3811c",
+    "url": "/static/css/main.774c0d22.chunk.css"
+  },
+  {
+    "revision": "fba757722f9a0b6b00d2",
+    "url": "/static/js/2.6d0e0ede.chunk.js"
+  },
+  {
+    "revision": "21f38193c3caf91fe32157540d33cdde",
+    "url": "/static/js/2.6d0e0ede.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e86c848160a82e3811c",
+    "url": "/static/js/main.ac96dcea.chunk.js"
+  },
+  {
+    "revision": "3a4ae1771cd5934e1ac9",
+    "url": "/static/js/runtime-main.6a25005e.js"
+  },
+  {
+    "revision": "b9cef175e98a5cc5be91963e39e0356a",
+    "url": "/static/media/Boston_Back_Bay_reflection_darker.b9cef175.jpg"
+  },
+  {
+    "revision": "42a40fe8e50bdab7bea3bc7f4c156071",
+    "url": "/static/media/Profilbild-min1.42a40fe8.jpg"
+  }
+]);
