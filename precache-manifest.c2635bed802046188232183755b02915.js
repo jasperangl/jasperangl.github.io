13aec3df75e067adf44f307fc2b3a0dc53f2ca6f
jasperangl <49351257+jasperangl@users.noreply.github.com>
2021-02-18 20:30:31 +0100

    Updates

--- precache-manifest.c2635bed802046188232183755b02915.js
+++ precache-manifest.c2635bed802046188232183755b02915.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7ad91e34d31fc46a2e173ee2454f977",
+    "revision": "239f265b7f364c0e5307b6bc3a20f393",
     "url": "/index.html"
   },
   {
-    "revision": "7d08f8a829290d05d958",
-    "url": "/static/css/main.5facd0c8.chunk.css"
+    "revision": "ed17b295282e467d3244",
+    "url": "/static/css/main.a23b06a0.chunk.css"
   },
   {
-    "revision": "4485fce3a9362f1bd3cb",
-    "url": "/static/js/2.d590ed76.chunk.js"
+    "revision": "f6c9e04acfffb18d7f8f",
+    "url": "/static/js/2.f2f3b4f5.chunk.js"
   },
   {
     "revision": "21f38193c3caf91fe32157540d33cdde",
-    "url": "/static/js/2.d590ed76.chunk.js.LICENSE"
+    "url": "/static/js/2.f2f3b4f5.chunk.js.LICENSE"
   },
   {
-    "revision": "7d08f8a829290d05d958",
-    "url": "/static/js/main.b9787f00.chunk.js"
+    "revision": "ed17b295282e467d3244",
+    "url": "/static/js/main.aaf6aad4.chunk.js"
   },
   {
     "revision": "0d46f7656c748826843c",
@@ -36,6 +36,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Minesweeper.36c9ac74.mp4"
   },
   {
+    "revision": "9bf92939fe7328f40b4891dd7bee53f3",
+    "url": "/static/media/MinesweeperImg.9bf92939.jpg"
+  },
+  {
     "revision": "42a40fe8e50bdab7bea3bc7f4c156071",
     "url": "/static/media/Profilbild-min1.42a40fe8.jpg"
   },
@@ -44,12 +48,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Pyramidsolitaire-final.b304513f.mp4"
   },
   {
+    "revision": "36ac1623477e1cf8c347f6675a198d9a",
+    "url": "/static/media/RecipeImg.36ac1623.png"
+  },
+  {
     "revision": "3d390fc2ae1f40733da5bfe52acbbce0",
     "url": "/static/media/Resume.3d390fc2.pdf"
   },
   {
     "revision": "0fa129c448e4562c012de5e1599b745e",
     "url": "/static/media/ShipGame.0fa129c4.mp4"
+  },
+  {
+    "revision": "3e8a8825ceb26f177c2d3061b41b2d76",
+    "url": "/static/media/ShipGameImg2.3e8a8825.jpg"
+  },
+  {
+    "revision": "53f810ef785260d1ed66819b28a7204c",
+    "url": "/static/media/Snake PvP Paper.53f810ef.pdf"
+  },
+  {
+    "revision": "3a2fd7343e7b1f6fa67de9abf6992688",
+    "url": "/static/media/Snake-PvP.3a2fd734.jpg"
+  },
+  {
+    "revision": "4899dd102267218d546f1b48e952343a",
+    "url": "/static/media/Spreadsheet-final.4899dd10.jpg"
   },
   {
     "revision": "f63a13b5183d0d4cb7cab166b72de3bd",
